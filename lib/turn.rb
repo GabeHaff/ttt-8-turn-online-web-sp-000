154993df3368a09bc2631f7dd07cdbe40e40382d
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-25 15:48:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ def turn(input)
   puts "Please enter 1-9:"
   input=gets.chomp 
    input_to_index(input)
-if valid_move?(board,index)==true 
+if valid_move?==true 
   return true
 else return false 
 end 
